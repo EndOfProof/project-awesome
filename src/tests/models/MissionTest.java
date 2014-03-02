5f8223f39f116e3b286f80049e1dd3f528cf005c
Mike <mikesb101@gmail.com>
2014-03-02 17:25:39 +0000

    adding the skeleton classes

---
+++ src/tests/models/MissionTest.java
@@ -0,0 +1,26 @@
+package tests.models;
+
+import static org.junit.Assert.*;
+import app.models.Mission;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class MissionTest {
+	private Mission mission;
+	
+	@Before
+	public void setUp() throws Exception {
+		mission = new Mission();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		
+	}
+
+}
